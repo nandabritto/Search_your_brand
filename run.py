5b08287a5c649b063b4386d4c605207698524405
nandabritto <nanda.britto@gmail.com>
2021-07-11 20:26:38 +0000

    Add input for user add keywords, date and language. Create a list of lat 20 tweets with user's keyword

--- run.py
+++ run.py
@@ -1,20 +1,42 @@
 # python code goes here
 import os
 import tweepy as tw
-import pandas as pd 
+import pandas as pd
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 consumer_key = os.environ.get('API_KEY')
-consumer_secret= os.environ.get('API_SECRET_KEY')
-access_token= os.environ.get('ACCESS_TOKEN')
-access_token_secret= os.environ.get('ACCESS_TOKEN_SECRET')
+consumer_secret = os.environ.get('API_SECRET_KEY')
+access_token = os.environ.get('ACCESS_TOKEN')
+access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
 
 auth = tw.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
 
+"""
+Code based on earthdatascience.org
+"""
 
-api.update_status("Look, I'm tweeting from #Python in my #earthanalytics class! @EarthLabCU")
+# Define the search term and the date_since date as input
+search_words = input("Add your keyword here: \n")
+date_since = input("Add your date range here: \n")
+language_search = input("Add your prefered language here: \n")
+
+new_search = search_words + "-filter:retweets"
+
+# Collect tweets
+tweets = tw.Cursor(
+              api.search,
+              q=new_search,
+              lang=language_search,
+              since=date_since
+                  ).items(20)
+
+tweets
+
+# Iterate and print tweets
+for tweet in tweets:
+    print(tweet.text, end='\n\n')
